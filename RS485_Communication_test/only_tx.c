dffe777962b0af69a7d24e93ecbf1c1d62d5e350
Takehiro Hasegawa <39427883+Takehiro-Hasegawa@users.noreply.github.com>
2021-10-20 14:40:43 +0900

    Update only_tx.c

--- RS485_Communication_test/only_tx.c
+++ RS485_Communication_test/only_tx.c
@@ -1,1 +1,11 @@
-test creation faile
+char data[7] = {};
+
+    data[0] = 0x07;
+    data[1] = 0x03;
+    data[2] = 0x00;
+    data[3] = ID;
+    data[4] = addr;
+    data[5] = length;
+    data[6] = 0;
+    for(int i = 0; i < data[0]-1; i++)
+        data[6] += data[i];
