165718c78fea88ed4647defec9fd94569657d9c9
yasuohayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2021-11-02 14:02:52 +0900

    b3m_test.c

---
+++ b3m_driver/b3m_test.c
@@ -0,0 +1,12 @@
+#include "b3m.h"
+
+int main(){
+	B3MData b3m;
+	char *serial_port = "/dev/tty";
+	if (b3m_init(&b3m, serial_port) < 0){
+		fprintf(stderr, "cannnot port open %s\r\n", serial_port);
+	}
+	b3m_servo_mode(&b3m, 0, B3M_OPTIONS_RUN_NORMAL);
+	b3m_set_angle(&b3m, 0, 0.0);
+	b3m_close(&b3m);
+}
